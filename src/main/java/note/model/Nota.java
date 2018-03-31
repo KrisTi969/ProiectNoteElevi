476f0eb4e63a1d8db1b1e7493f4f2813cdf47f80
KrisTi969 <crys_gold_ten@yahoo.ca>
2018-03-31 12:47:36 +0300

    some tests

--- src/main/java/note/model/Nota.java
+++ src/main/java/note/model/Nota.java
@@ -1,15 +1,65 @@
 package note.model;
 
+import com.sun.media.sound.InvalidFormatException;
+
 public class Nota {
 	
-	private int nrmatricol;
+	private Integer nrmatricol;
 	private String materie;
 	private double nota;
 	
-	public Nota(int nrmatricol, String materie, double nota) {
+	public Nota(Integer nrmatricol, String materie, double nota) {
 		this.setNrmatricol(nrmatricol);
 		this.setMaterie(materie);
 		this.setNota(nota);
+	}
+	public Nota(String nrmatricol, String materie, String nota) throws Exception {
+		if(materie.equals("")) throw new Exception("materie cannont be null");
+		if(nrmatricol.equals("")) throw new Exception("nrmatricol cannont be null");
+		if(validNrMatricol(nrmatricol)) throw new Exception("Invalid nrmatricol");
+		if(validNota(nota)) throw new Exception("Invalid nota");
+		if(validMaterie(materie)) throw new Exception("Materie has invalid type");
+
+		this.setNrmatricol(Integer.parseInt(nrmatricol));
+		this.setMaterie(materie);
+		this.setNota(Integer.parseInt(nota));
+	}
+
+	private static boolean validNota(String str) {
+		Integer integer;
+		try {
+			integer = Integer.parseInt(str);
+		}catch (NumberFormatException e) {
+			return true;
+		}
+		if (integer > 10 && integer < 1) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean validMaterie(String str) {
+		Integer integer;
+		try {
+			integer = Integer.parseInt(str);
+		}catch (NumberFormatException e) {
+			return true;
+		}
+		return false;
+	}
+
+
+	private static boolean validNrMatricol(String str) {
+		Integer integer ;
+try {
+	 integer = Integer.parseInt(str);
+}catch (NumberFormatException e) {
+	return true;
+}
+		if (integer > 1000 && integer < 1) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
@@ -22,7 +72,7 @@ public class Nota {
 	/**
 	 * @param nrmatricol the nrmatricol to set
 	 */
-	public void setNrmatricol(int nrmatricol) {
+	public void setNrmatricol(Integer nrmatricol) {
 		this.nrmatricol = nrmatricol;
 	}
 
--- src/test/java/note/test/RepositoryNoteTest.java
+++ src/test/java/note/test/RepositoryNoteTest.java
@@ -30,19 +30,105 @@ public class RepositoryNoteTest {
         noteRepository.addNota(nota);
         assertTrue(repoSizeBeforeAdd + 1 == noteRepository.count());
     }
-   /* @Test
-    public void addNrMatricolInvalid() {
+    @Test
+    public void addNrMatricolInvalid() throws Exception {
 
         try {
-            nota = new Nota(1, "Istorie", 7);
-            nota.setNota("");
-        } catch (Exception e) {
-            assertTrue(true);
-            System.out.println("Name can't be string");
-        }
+            nota = new Nota("", "Istorie", "7");
 
-        int repoSizeBeforeAdd = repositoryContact.count();
-        repositoryContact.addContact(contact);
-        assertTrue(repoSizeBeforeAdd + 1 != repositoryContact.count());
-    }*/
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println("Nr matricol nu poate fi empty");
+        }
+        int repoSizeBeforeAdd = noteRepository.count();
+        try {
+            noteRepository.addNota(nota);
+        } catch (NullPointerException e) {
+            assertTrue(repoSizeBeforeAdd + 1 != noteRepository.count());
+        }
+    }
+
+
+    @Test
+    public void addNrMatricolInvalidType() throws Exception {
+
+        try {
+            nota = new Nota("Marcel", "Istorie", "7");
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println("Nr matricol nu poate fi string");
+        }
+        int repoSizeBeforeAdd = noteRepository.count();
+        try {
+            noteRepository.addNota(nota);
+        } catch (NullPointerException e) {
+            assertTrue(repoSizeBeforeAdd + 1 != noteRepository.count());
+        }
+    }
+
+    @Test
+    public void addNrMatricolInvalidLength() throws Exception {
+
+        try {
+            nota = new Nota("1230213123", "Istorie", "7");
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println("Nr matricol has invalid legth");
+        }
+        int repoSizeBeforeAdd = noteRepository.count();
+        try {
+            noteRepository.addNota(nota);
+        } catch (NullPointerException e) {
+            assertTrue(repoSizeBeforeAdd + 1 != noteRepository.count());
+        }
+    }
+
+    @Test
+    public void addmaterieEmplty() throws Exception {
+
+        try {
+            nota = new Nota("1000", "", "7");
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println("Materie nu poate fi empty");
+        }
+        int repoSizeBeforeAdd = noteRepository.count();
+        try {
+            noteRepository.addNota(nota);
+        } catch (NullPointerException e) {
+            assertTrue(repoSizeBeforeAdd + 1 != noteRepository.count());
+        }
+    }
+    @Test
+    public void addMaterieInvalidType() throws Exception {
+        try {
+            nota = new Nota("1000", "123wqe", "7");
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println("Materie has invalid type");
+        }
+        int repoSizeBeforeAdd = noteRepository.count();
+        try {
+            noteRepository.addNota(nota);
+        } catch (NullPointerException e) {
+            assertTrue(repoSizeBeforeAdd + 1 != noteRepository.count());
+        }
+    }
+
+    @Test
+    public void addNotaEquals0() throws Exception {
+        try {
+            nota = new Nota("1000", "Istorie", "0");
+            assertTrue(true);
+        } catch (Exception e) {
+            System.out.println("Nota nu poate fi 0");
+        }
+        int repoSizeBeforeAdd = noteRepository.count();
+        try {
+            noteRepository.addNota(nota);
+        } catch (Exception e) {
+            assertTrue(repoSizeBeforeAdd + 1 != noteRepository.count());
+        }
+    }
+
 }
